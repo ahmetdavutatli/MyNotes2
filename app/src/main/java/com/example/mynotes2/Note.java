6c29c55fb8af1d34ee3f1a433cae845212850596
ahmetdavutatli <ahmetdavutatli@posta.mu.edu.tr>
2022-12-18 09:29:42 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mynotes2/Note.java
@@ -0,0 +1,35 @@
+package com.example.mynotes2;
+
+
+import com.google.firebase.Timestamp;
+
+public class Note {
+
+    private String id;
+    private Timestamp date;
+    private String content;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public void setDate(Timestamp date) {
+        this.date = date;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
